719550ded537436475a8a08d6e532a45ce8ce2c2
AyushBipinPatel <ayush.ap58@gmail.com>
2022-05-04 11:48:06 +0530

    1> removed the data folder. 2> typo correction in module what iff

--- R/mod_country_level_growth.R
+++ R/mod_country_level_growth.R
@@ -10,7 +10,31 @@
 mod_country_level_growth_ui <- function(id){
   ns <- NS(id)
   tagList(
-    add_instruction_explation_text_whatiff()
+    add_instruction_explation_text_whatiff(),
+    shiny::sidebarLayout(
+      sidebarPanel = shiny::sidebarPanel(
+        shinyWidgets::sliderTextInput(
+          inputId = ns("choice_year"),
+          label = "Choose Year",
+          choices = seq(1960,2019,by = 1),
+          selected = 1960,
+          grid = T
+        ),
+        shinyWidgets::sliderTextInput(
+          inputId = ns("choice_rate"),
+          label = "Choose rate of growth (%)",
+          choices = seq(1,20,by = 0.5),
+          selected = 10,
+          grid = T
+          )
+      ),
+      mainPanel = shiny::mainPanel(
+        shiny::tabsetPanel(
+          shiny::tabPanel(title = "GDP Trends"),
+          shiny::tabPanel(title = "GDP per Capita Trends")
+        )
+      )
+                           )
   )
 }
 
