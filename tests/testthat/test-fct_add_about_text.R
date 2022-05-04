794b1915e80b8f0388987399d5eed83f10ed5850
AyushBipinPatel <ayush.ap58@gmail.com>
2022-05-04 16:19:23 +0530

    tests for functions

--- tests/testthat/test-fct_add_about_text.R
+++ tests/testthat/test-fct_add_about_text.R
@@ -1,3 +1,3 @@
 test_that("Function to add intro text in about page works", {
-  golem::expect_shinytag(add_about_test())
+  golem::expect_shinytag(add_about_text())
 })
--- tests/testthat/test-fct_add_instruction_explation_text_whatiff.R
+++ tests/testthat/test-fct_add_instruction_explation_text_whatiff.R
@@ -1,3 +1,3 @@
 test_that("Function that adds instruction and expalnation to the What iff section", {
-  golem::expect_shinytag(add_instruction_explation_text_whatiff())
+  golem::expect_html_equal(add_instruction_explation_text_whatiff())
 })
