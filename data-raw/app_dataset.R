601804304f9840a5e1279350cf79da36f44f421d
AyushBipinPatel <ayush.ap58@gmail.com>
2022-06-14 11:53:19 +0530

    changes to sysdata for inicluding forward looking india gdp and gdp per capita

--- data-raw/app_dataset.R
+++ data-raw/app_dataset.R
@@ -17,7 +17,17 @@ read_csv("data-raw/1_data_gdp_and_percapita_gdp.csv") -> data_gdp
 # create proper column columns ----------------------------------------
 
 
-data_gdp %>% janitor::clean_names() -> data_gdp
+data_gdp %>% janitor::clean_names() -> data_gdp # India national level data for gdp and gdp per capita
+
+rbind(
+  data_gdp,
+  tibble(
+    year = c(2021:2050),
+    total_population = 1380004385,
+    gdp_current_usd = NA,
+    gdp_per_capita_current_usd = NA
+  )
+) -> data_gdp_future # India National level data with years till 2050 for future time series charts. Has GDP and GDp per capita
 
 
 # write to sysdata --------------------------------------------------------
