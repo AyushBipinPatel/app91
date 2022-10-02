d90b1dd47ed64429b1d0387e492e847a63c2e1d7
AyushBipinPatel <ayush.ap58@gmail.com>
2022-10-02 15:28:12 +0530

    data prep script for india states gdp and gdppc. Uppdate sys data of the app

--- data-raw/app_dataset.R
+++ data-raw/app_dataset.R
@@ -4,6 +4,7 @@
 
 # library -----------------------------------------------------------------
 
+library(here)
 library(dplyr)
 library(readr)
 
@@ -15,6 +16,19 @@ library(readr)
 read_csv("data-raw/1_data_gdp_and_percapita_gdp.csv") -> data_gdp
 # 15 countries of interest with their national level gdppc data
 read.csv("data-raw/7_data_countries_interest_compare_gdp_per_capita_forward.csv") -> data_gdppc_countries
+
+# State gdp data for india
+
+read.csv(here("data-raw/9_1_data_epw_series_sdp_net.csv")) -> india_states_gdp
+
+janitor::clean_names(india_states_gdp) -> india_states_gdp
+
+# Stae gdppc data for india
+
+read.csv(here("data-raw/10_1_data_epw_series_per_capita_sdp_net.csv")) -> india_states_gdppc
+
+janitor::clean_names(india_states_gdppc) -> india_states_gdppc
+
 
 # data prep ----------------------------------------
 
@@ -35,4 +49,7 @@ rbind(
 # write to sysdata --------------------------------------------------------
 
 
-usethis::use_data(data_gdp,data_gdppc_countries,data_gdp_future,overwrite = T,internal = T)
+usethis::use_data(data_gdp,data_gdppc_countries,
+                  data_gdp_future,india_states_gdp,
+                  india_states_gdppc,
+                  overwrite = T,internal = T)
