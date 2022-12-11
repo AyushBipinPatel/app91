be8e9c463ce4466742cc0a52fc3851ce2fc9b6bb
AyushBipinPatel <ayush.ap58@gmail.com>
2022-12-11 19:26:17 +0530

    changes due to removing user level controls on referecen countries

--- R/mod_country_level_growth_extrapolate.R
+++ R/mod_country_level_growth_extrapolate.R
@@ -25,26 +25,6 @@ mod_country_level_growth_extrapolate_ui <- function(id){
         shiny::actionButton(
           inputId = ns("set_changes"),
           label = "Change Growth Rate"
-        ),
-        shiny::tags$hr(),
-        shiny::tags$p(shiny::tags$b("Add a reference threshold for GDP/GDPP")),
-        shinyWidgets::pickerInput(
-          inputId = ns("choice_country_compare"),
-          label = "Pick a country to compare",
-          choices = unique(data_gdppc_countries$country_name),
-          selected = "Australia",
-          options = list(`live-search` = TRUE)
-        ),
-        shinyWidgets::sliderTextInput(
-          inputId = ns("choice_country_year"),
-          label = "Choose year for GDP/GDP per capita",
-          choices = seq(1960,2020,by = 1),
-          selected = 1990,
-          grid = T
-        ),
-        shiny::actionButton(
-          inputId = ns("set_country_changes"),
-          label = "Add refrence to compare"
         )
         ),
       mainPanel = shiny::mainPanel(
@@ -95,79 +75,196 @@ mod_country_level_growth_extrapolate_server <- function(id){
     },ignoreNULL = F)
 
 
-    # section to control Dynamic UI for the  forward looking  comparis --------
+    # function to extract detials from data_cc
 
-    shiny::observeEvent(input$choice_country_compare,{
-      shinyWidgets::updateSliderTextInput(session = session,
-        inputId = "choice_country_year",
-        choices = seq(min(data_gdppc_countries$year[data_gdppc_countries == input$choice_country_compare & (!is.na(data_gdppc_countries$gdp_per_capita))],na.rm =T),
-                      max(data_gdppc_countries$year[data_gdppc_countries == input$choice_country_compare & (!is.na(data_gdppc_countries$gdp_per_capita))],na.rm =T),
-                      by = 1)
+    get_from_data_cc <- function(measure_passed,value_passed,detail_extract){
+
+      data_cc[[detail_extract]][which.min(abs(data_cc[[measure_passed]] - value_passed))]
+
+
+    }
+
+
+    #get the reference data prepared for gdppc/gdp of a countries --------------------
+
+    shiny::reactive({
+      tibble::tibble(
+        ind_gdp = data_reactive() %>%
+          dplyr::filter(year %in% c(2025,2030,2035,2040,2045,2050)) %>%
+          dplyr::pull(trend_gdp),
+        year = c(2025,2030,2035,2040,2045,2050),
+        ind_gdppc = data_reactive() %>%
+          dplyr::filter(year %in% c(2025,2030,2035,2040,2045,2050)) %>%
+          dplyr::pull(trend_gdp_per_capita),
+        cc_gdp = purrr::pmap_dbl(.l = list(
+          measure_passed = "gdp",
+          value_passed = ind_gdp,
+          detail_extract = "gdp"
+        ) ,.f = get_from_data_cc),
+        cc_year_gdp = purrr::pmap_dbl(.l = list(
+          measure_passed = "gdp",
+          value_passed = ind_gdp,
+          detail_extract = "year"
+        ) ,.f = get_from_data_cc),
+        cc_country_gdp = purrr::pmap_chr(.l = list(
+          measure_passed = "gdp",
+          value_passed = ind_gdp,
+          detail_extract = "country_name"
+        ) ,.f = get_from_data_cc),
+        cc_gdppc = purrr::pmap_dbl(.l = list(
+          measure_passed = "gdppc",
+          value_passed = ind_gdppc,
+          detail_extract = "gdppc"
+        ) ,.f = get_from_data_cc),
+        cc_year_gdppc = purrr::pmap_dbl(.l = list(
+          measure_passed = "gdppc",
+          value_passed = ind_gdppc,
+          detail_extract = "year"
+        ) ,.f = get_from_data_cc),
+        cc_country_gdppc =  purrr::pmap_chr(.l = list(
+          measure_passed = "gdppc",
+          value_passed = ind_gdppc,
+          detail_extract = "country_name"
+        ) ,.f = get_from_data_cc)
       )
-    },ignoreNULL = F)
-
-
-    # set the reference gdppc of a country as reactive val --------------------
-
-        compare_country <- shiny::eventReactive(input$set_country_changes,{
-
-          input$choice_country_compare
-
-        },ignoreNULL = F)
-
-        compare_country_year <- shiny::eventReactive(input$set_country_changes,{
-
-          input$choice_country_year
-
-        },ignoreNULL = F)
-
-        reference_gdppc <- reactive({
-
-          data_gdppc_countries$gdp_per_capita[data_gdppc_countries$country_name == compare_country() &
-                                                data_gdppc_countries$year == compare_country_year()]
-
-        })
-
-        reference_gdp <- reactive({
-
-          data_gdp_countries$gdp[data_gdp_countries$`Country Name` == compare_country() &
-                                                data_gdp_countries$Year == compare_country_year()]
-
-        })
-
-
-
+    }) -> data_cc_plot
 
 
     # create charts -----------------------------------------------------------
 
     output$line_gdp <- highcharter::renderHighchart({
 
-      add_create_comparitive_time_series_chart(
-        data_fetch = data_reactive(),
-        xval = "gdp_current_usd",
-        yval = "trend_gdp",
-        plt_title = "Actual GDP trend and the Extrapolated GDP trend",
-        flname = "actual_vs_extrapolated_gdp",
-        labx = "Actual GDP trend",
-        laby = "Extrapolated GDP trend",
-        ref_val_gdp = reference_gdp()
-      )
+      highcharter::hchart(data_reactive(), "line",
+                          highcharter::hcaes("year", "gdp_current_usd"),
+                          color = "#e7eced",  name = "Actual GDP trend",
+                          tooltip = list(
+                            pointFormatter = htmlwidgets::JS(
+                              "
+                                       function(){
+                                       return this.series.name + ': ' + this.y/1000000000 + 'B'
+                                       }
+                                       "
+                            )
+                          )) %>%
+        highcharter::hc_add_series(data = data_reactive(),type = "line",
+                                   highcharter::hcaes("year","trend_gdp"),
+                                   name = "Extrapolated GDP",
+                                   color = "#ffc051",
+                                   tooltip = list(
+                                     pointFormatter = htmlwidgets::JS(
+                                       "
+                                       function(){
+                                       return this.series.name + ': ' + this.y/1000000000 + 'B'
+                                       }
+                                       "
+                                     )
+                                   )) %>%
+        highcharter::hc_add_series(data = data_cc_plot(),
+                                   type = "column",
+                                   pointWidth = 0.1,
+                                   highcharter::hcaes("year","cc_gdp"),
+                                   name = "Comparative Countries",
+                                   color = "#898999",
+                                   tooltip = list(
+                                     pointFormatter = htmlwidgets::JS(
+                                       "
+                                       function(){
+                                       return '<br>GDP of ' + this.cc_country_gdp + ' in ' + this.cc_year_gdp + ': ' + this.y/1000000000 + 'B'
+                                       }
+                                       "
+                                       )
+                                   )
+                                   ) %>%
+        highcharter::hc_exporting(
+          enabled = TRUE,
+          filename = "actual_vs_extrapolated_gdp"
+        ) %>%
+        highcharter::hc_title(
+          text = "Actual GDP trend and the Extrapolated GDP trend",
+          margin = 20,
+          align = "left",
+          style = list(color = "#22A884", useHTML = TRUE)
+        )  %>%
+        highcharter::hc_xAxis(title = list(text = "Year")) %>%
+        highcharter::hc_yAxis(title = list(text =  "GDP in Bilions USD"),
+                              labels = list(formatter = htmlwidgets::JS("function(){
+                                                        return this.value /1000000000 + 'B'
+                          }"),
+                          format = "${text}"
+                              )
+        ) %>%
+        highcharter::hc_tooltip(
+          shared = T,
+          crosshairs = T
+        )
+
     })
 
 
     output$line_gdp_per_capita <- highcharter::renderHighchart({
 
-      add_create_comparitive_time_series_chart(
-        data_fetch = data_reactive(),
-        xval = "gdp_per_capita_current_usd",
-        yval = "trend_gdp_per_capita",
-        plt_title = "Actual GDP per capita trend compared to the Extrapolated GDP per capita trend",
-        flname = "actual_vs_extrapolated_gdp_per_capita",
-        labx = "Actual GDP per capita trend",
-        laby = "Extrapolated GDP per capita trend",
-        ref_val_gdppc = reference_gdppc()
-      )
+      highcharter::hchart(data_reactive(), "line",
+                          highcharter::hcaes("year","gdp_per_capita_current_usd" ),
+                          color = "#e7eced",
+                          name = "Actual GDP per capita trend",
+                          tooltip = list(
+                            pointFormatter = htmlwidgets::JS(
+                              "
+                                       function(){
+                                       return this.series.name + ': ' + this.y + 'USD'
+                                       }
+                                       "
+                            )
+                          )) %>%
+        highcharter::hc_add_series(data = data_reactive(),type = "line",
+                                   highcharter::hcaes("year","trend_gdp_per_capita"),
+                                   name = "Extrapolated GDP per capita trend",
+                                   color = "#ffc051",
+                                   tooltip = list(
+                                     pointFormatter = htmlwidgets::JS(
+                                       "
+                                       function(){
+                                       return this.series.name + ': ' + this.y + 'USD'
+                                       }
+                                       "
+                                     )
+                                   )) %>%
+        highcharter::hc_add_series(data = data_cc_plot(),
+                                   type = "column",
+                                   pointWidth = 0.1,
+                                   highcharter::hcaes("year","cc_gdppc"),
+                                   name = "Comparative Countries",
+                                   color = "#898999",
+                                   tooltip = list(
+                                     pointFormatter = htmlwidgets::JS(
+                                       "
+                                       function(){
+                                       return '<br>GDP per capita of ' + this.cc_country_gdppc + ' in ' + this.cc_year_gdppc + ': ' + this.y + 'USD'
+                                       }
+                                       "
+                                     )
+                                   )
+        ) %>%
+        highcharter::hc_exporting(
+          enabled = TRUE,
+          filename = "actual_vs_extrapolated_gdp_per_capita"
+        ) %>%
+        highcharter::hc_title(
+          text = "Actual GDP per capita trend compared to the Extrapolated GDP per capita trend",
+          margin = 20,
+          align = "left",
+          style = list(color = "#22A884", useHTML = TRUE)
+        )  %>%
+        highcharter::hc_xAxis(title = list(text = "Year")) %>%
+        highcharter::hc_yAxis(title = list(text = "GDP per capita in USD"),
+                              labels = list(formatter =
+                                htmlwidgets::JS("function(){return this.value + 'USD'}"),
+                              format = "${text}")
+        ) %>%
+        highcharter::hc_tooltip(
+          shared = T,
+          crosshairs = T
+        )
     })
 
   })
